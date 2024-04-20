1fffc17478a35fe28389be2c43ebfe0061bfafc9
Witaya252525 <witaya.c@kkumail.com>
2024-04-21 06:46:05 +0700

    add details products

--- djangoworkshop/store/views.py
+++ djangoworkshop/store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from store.models import Category , Product
 # from django.http import HttpResponse
 
 
@@ -6,7 +7,12 @@ from django.shortcuts import render
 #     return HttpResponse('<h1> Hello Django  </h1>')
 
 def index(request):
-    return render (request , 'index.html')
+    products=None
+    products=Product.objects.all().filter(available=True)
+    return render (request , 'index.html' ,{'products':products})
+
+
+
 
 
 def product(request):
